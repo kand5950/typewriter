5791f643e8cd40ca0c71298053d1d3fdfda1f367
Anit <anittankandasamy@gmail.com>
2022-09-27 21:50:18 +0000

    printing each word of string in between 1 second

---
+++ hello_timeout.js
@@ -0,0 +1,15 @@
+setTimeout(() => {
+  process.stdout.write("43llo ");
+}, 1000);
+setTimeout(() => {
+  process.stdout.write("th3r3");
+}, 2000);
+setTimeout(() => {
+  process.stdout.write(", ");
+}, 2400);
+setTimeout(() => {
+  process.stdout.write("w0rld.");
+}, 3400);
+setTimeout(() => {
+  console.log()
+}, 5000);
